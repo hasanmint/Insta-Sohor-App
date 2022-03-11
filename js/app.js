3578f4961f53ab150657f5b0a6a1d87ba6f36d5e
hasanmint73 <hasanmint73@gmail.com>
2022-03-11 07:50:03 +0600

    Each Post username and  the comment are show. bug 4  fixed

--- js/app.js
+++ js/app.js
@@ -54,6 +54,9 @@ const createPost = (post) => {
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
+    console.log(post.comments[0].user);
+    console.log(post.comments[0].text);
+    
     div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -120,9 +123,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
